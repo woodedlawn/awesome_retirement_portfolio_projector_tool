e87e810655b86aa550265233b07a82413a886022
paocarvajal1912 <paola.antonieta@gmail.com>
2021-08-15 22:44:41 -0400

    combine API and optimization parts in jon&pao folder

---
+++ jon&pao/db.py
@@ -0,0 +1,53 @@
+import sqlalchemy as sql
+
+def get_db():
+  connection_string = 'sqlite:///awesome_app.db'
+  engine = sql.create_engine(connection_string, echo=False)
+  
+  return engine
+
+
+
+def update_portfolio(portfolio):
+  engine = get_db()
+
+  engine.execute("DROP TABLE IF EXISTS portfolio")
+  engine.execute("""
+    CREATE TABLE portfolio (
+      symbol VARCHAR,
+      quantity REAL,
+      type VARCHAR
+    )
+  """)
+
+  insert_portfolio_sql = """
+    INSERT INTO portfolio ("symbol", "quantity", "type")
+    VALUES 
+  """
+
+  count = 0
+  for asset in portfolio:
+    if count > 0:
+      insert_portfolio_sql += ", "  
+    insert_portfolio_sql += f"(\"{asset['symbol']}\", \"{asset['quantity']}\", \"{asset['type']}\")"
+    count += 1
+  
+  engine.execute(insert_portfolio_sql)
+
+
+
+def update_report(report_details):
+  engine = get_db()
+
+  engine.execute("DROP TABLE IF EXISTS report")
+  engine.execute("""
+    CREATE TABLE report (
+      risk_tolerance VARCHAR,
+      years_until_retirement REAL
+    )
+  """)
+  engine.execute(f"""
+    INSERT INTO report ("risk_tolerance", "years_until_retirement")
+    VALUES ("{report_details['risk_tolerance']}", "{report_details['years_until_retirement']}")
+  """)
+
